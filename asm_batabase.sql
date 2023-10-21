882f77c12a121f76ca405038b40d0759224044a8
Dev <hoangtv1809@gmail.com>
2023-10-21 16:44:22 +0700

    Update

--- asm_batabase.sql
+++ asm_batabase.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th10 11, 2023 lúc 05:36 AM
+-- Thời gian đã tạo: Th10 19, 2023 lúc 03:11 PM
 -- Phiên bản máy phục vụ: 10.4.28-MariaDB
 -- Phiên bản PHP: 8.2.4
 
@@ -75,7 +75,7 @@ INSERT INTO `catalog` (`id`, `name`, `number`, `img`) VALUES
 (2, 'Laptop', 0, '\r\n'),
 (3, 'Tablet', 0, ''),
 (4, 'Tai Nghe', 0, NULL),
-(22, 'Watch', 0, NULL);
+(5, 'Watch', 0, NULL);
 
 -- --------------------------------------------------------
 
@@ -116,23 +116,23 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`id`, `name`, `img`, `price`, `price_sale`, `describe`, `specifications`, `new`, `promotion`, `view`, `id_catalog`) VALUES
-(1, 'iPhone 14 Pro Max', 'iphone-14-pro.png', 1114.09, 1266.07, '', '', 1, 12, 0, 1),
-(2, 'Realme 9 Pro Plus', 'realme_9_pro_plus.jpg', 240.21, 295.09, '', '', 0, 6, 0, 1),
-(3, 'Samsung Galaxy S22 Ultra', 'samsung_galaxys22ultra.jpg', 759.47, 1308.29, '', '', 0, 42, 0, 1),
-(4, 'iPad Pro M2', 'ipad-pro-129-m2.jpg', 852.35, 1012.77, '', '', 0, 16, 0, 3),
-(5, 'iPad Pro M1', 'ipad-pro-m1.png', 801.69, 1012.77, '', '', 0, 21, 0, 3),
-(6, 'Samsung Galaxy A32\r\n', 'Samsung-Galaxy-a32-5G.png', 172.67, 273.98, '', '', 0, 37, 0, 1),
-(7, 'Masstel Tab 10', 'masstel-tab-10s-_en-1-1-2-3-4-60.jpg', 71.35, 0.00, '', '', 0, 0, 0, 3),
-(9, 'Laptop Acer Swift 3 SF314', 'acer-swift-3-sf314.jpg', 675.04, 886.12, '', '', 0, 24, 0, 2),
-(10, 'Laptop MSI Gaming GF63 ', 'msi-gaming-gf63-thin-11.jpg', 632.62, 717.03, '', '', 0, 9, 0, 2),
-(11, 'Tai nghe chụp tai Gaming Havit H2012D RGB', 'havit_h2012d_3.jpg', 20.72, 24.90, '', '', 0, 17, 0, 4),
-(25, 'Apple Watch SE 2022 40mm', 'apple_watch.jpg', 21.02, 25.24, '', '', 0, 20, 0, 22),
-(26, 'Laptop Dell Gaming G15 5530 i7', 'laptop-dell-alienware-m15-r5-2.jpg', 1726.90, 1811.25, '', '', 0, 5, 0, 2),
-(28, 'Samsung Galaxy Z Flip4', 'samsung_galaxy_z_flip.jpg', 700.00, 750.00, '', '', 0, 10, 0, 1),
-(29, 'Samsung Tab S9', 'samsung_tab_s9.jpg', 996.36, 1000.00, '', '', 0, 20, 0, 3),
-(30, 'Samsung Galaxy Z Fold4', 'samsung_galaxy_z_fold_4.jpg', 608.70, 700.00, '', '', 0, 21, 0, 1),
-(31, 'Apple Watch Series 8 45mm', 'apple_watch1_258.jpg', 348.57, 512.61, '', '', 0, 70, 0, 22),
-(32, 'Tai nghe Gaming Asus Rog Strix Fusion 500', 'asus_rog_strix_fusion_500_3.jpg', 102.52, 225.55, '', '', 0, 50, 0, 4);
+(1, 'iPhone 14 Pro Max', 'iphone-14-pro.png', 26.79, 29.99, '', '', 1, 12, 0, 1),
+(2, 'Realme 9 Pro Plus', 'realme_9_pro_plus.jpg', 5.59, 6.99, '', '', 0, 6, 0, 1),
+(3, 'Samsung Galaxy S22 Ultra', 'samsung_galaxys22ultra.jpg', 17.99, 33.99, '', '', 0, 42, 0, 1),
+(4, 'iPad Pro M2', 'ipad-pro-129-m2.jpg', 20.09, 23.99, '', '', 1, 16, 0, 3),
+(5, 'iPad Pro M1', 'ipad-pro-m1.png', 18.99, 23.99, '', '', 0, 21, 0, 3),
+(6, 'Samsung Galaxy A32', 'Samsung-Galaxy-a32-5G.png', 4.09, 6.49, '', '', 0, 37, 0, 1),
+(7, 'Masstel Tab 10', 'masstel-tab-10s-_en-1-1-2-3-4-60.jpg', 2.49, 0.00, '', '', 0, 0, 0, 3),
+(8, 'Apple Watch SE 2022 40mm', 'apple_watch.jpg', 5.89, 7.49, '', '', 0, 20, 0, 5),
+(9, 'Laptop Acer Swift 3 SF314', 'acer-swift-3-sf314.jpg', 16.99, 22.99, '', '', 0, 24, 0, 2),
+(10, 'Laptop MSI Gaming GF63 ', 'msi-gaming-gf63-thin-11.jpg', 14.99, 16.99, '', '', 0, 9, 0, 2),
+(11, 'Tai nghe chụp tai Gaming Havit H2012D RGB', 'havit_h2012d_3.jpg', 39.00, 79.00, '', '', 0, 17, 0, 4),
+(12, 'Laptop Dell Gaming G15', 'laptop-dell-alienware-m15-r5-2.jpg', 18.49, 25.99, '', '', 1, 5, 0, 2),
+(13, 'Samsung Galaxy Z Flip4', 'samsung_galaxy_z_flip.jpg', 13.49, 23.99, '', '', 0, 10, 0, 1),
+(14, 'Samsung Tab S9', 'samsung_tab_s9.jpg', 19.99, 21.99, '', '', 0, 20, 0, 3),
+(15, 'Samsung Galaxy Z Fold4', 'samsung_galaxy_z_fold_4.jpg', 22.99, 40.99, '', '', 1, 21, 0, 1),
+(16, 'Apple Watch Series 8 45mm', 'apple_watch1_258.jpg', 8.29, 12.99, '', '', 1, 12, 0, 5),
+(17, 'Tai nghe Gaming Asus Rog Strix Fusion 500', 'asus_rog_strix_fusion_500_3.jpg', 26.90, 0.00, '', '', 0, 0, 0, 4);
 
 -- --------------------------------------------------------
 
@@ -142,13 +142,13 @@ INSERT INTO `product` (`id`, `name`, `img`, `price`, `price_sale`, `describe`, `
 
 CREATE TABLE `user` (
   `id` int(10) NOT NULL,
-  `username` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `phonenumber` int(10) NOT NULL DEFAULT 0,
-  `role` tinyint(1) NOT NULL DEFAULT 0,
-  `address` varchar(100) NOT NULL
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `phonenumber` int(10) DEFAULT 0,
+  `role` tinyint(1) DEFAULT 0,
+  `address` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -156,9 +156,10 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `password`, `name`, `email`, `phonenumber`, `role`, `address`) VALUES
-(1, 'hoang', '2004', 'Huỳnh Hoàng', 'hoang@gmail.com', 112434435, 1, 'TP.HCM'),
+(1, 'hoanghm', '123456789', 'Huỳnh Hoàng', 'hoang@gmail.com', 112434435, 1, 'TP.HCM'),
 (3, 'kurosato', '12345678', 'Kurosato', 'kuro123@gmail.com', 999999999, 0, 'Trà Vinh'),
-(4, 'minhhoang', '1809', 'Minh Hoàng', 'hoang123@gmail.com', 123456789, 0, 'TP.Trà Vinh');
+(4, 'minhhoang', '18092307', 'Minh Hoàng', 'hoang123@gmail.com', 123456789, 0, 'TP.Trà Vinh'),
+(15, 'huynhhoang', '18092004', 'Huỳnh Hoàng', 'hoangtv1809@gmail.com', 1122334455, 0, 'Tp.Trà Vinh');
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -244,7 +245,7 @@ ALTER TABLE `product`
 -- AUTO_INCREMENT cho bảng `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- Các ràng buộc cho các bảng đã đổ
