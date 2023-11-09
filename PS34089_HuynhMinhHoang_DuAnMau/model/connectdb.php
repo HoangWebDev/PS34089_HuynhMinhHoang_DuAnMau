9a94605e46cae38cfcb587294d68751900c6b304
Dev <hoangtv1809@gmail.com>
2023-11-09 21:16:25 +0700

    update

--- PS34089_HuynhMinhHoang_DuAnMau/model/connectdb.php
+++ PS34089_HuynhMinhHoang_DuAnMau/model/connectdb.php
@@ -6,7 +6,7 @@ function connectdb()
     $password = "";
 
     try {
-        $conn = new PDO("mysql:host=$servername;dbname=asm_batabase", $username, $password);
+        $conn = new PDO("mysql:host=$servername;dbname=asm_database", $username, $password);
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         //echo "Connected successfully";
